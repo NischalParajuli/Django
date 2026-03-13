ad6a6caa670f728d7745282e4b9764fad8629759
NischalParajuli <parajulinischal204@gmail.com>
2026-03-13 09:32:45 +0545

    Extending using base.html"

--- abc_app/views.py
+++ abc_app/views.py
@@ -4,18 +4,60 @@ from django.http import HttpResponse
 
 
 def home(request):
-    context = {
-        "title": "Home Title",
-        "body": "Home Page"
-    }
-    return render(request, 'home.html', context)
+   people = [
+      {
+         "name":"ram",
+         "age":"25",
+         "contact":"98546225555"
+      },
+      {
+         "name":"abc",
+         "age":"60",
+         "contact":"98546225555"
+      },
+      {
+         "name":"ert",
+         "age":"50",
+         "contact":"98546225555"
+      },
+      {
+         "name":"ffg",
+         "age":"30",
+         "contact":"98546225555"
+      },
+      {
+         "name":"ffg",
+         "age":"30",
+         "contact":"98546225555"
+      },
+      {
+         "name":"ffg",
+         "age":"30",
+         "contact":"98546225555"
+      }
+   ]
+   context = {
+      "title": "HOME Page",
+      "body":"!!!!",
+      "PEOPLE": people
+   }
+   return render(request,'home.html',context)
 
 # home()
 
 
 def about_us(request):
-    return render(request, 'about_us.html')
+    context = {
+        "title":"About-us",
+        "body":"I am Nischal"
+    }
+    return render(request, 'about_us.html', context)
+   
 
 
 def contact_us(request):
-    return render(request, 'contact_us.html')
+    context = {
+        "title":"contact-us",
+        "body":"parajulinischal204@gmail.com"
+    }
+    return render(request, 'contact_us.html', context)
